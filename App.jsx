25899700c9d897b1ead20b771411a9bfb1720c3f
majidmugal <majidmugal7737713356@gmail.com>
2026-05-16 17:47:42 +0530

    main

---
+++ App.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+// import Counter from "./Counter";
+import "./App.css";
+// import Form2 from "./Form2";
+import Login from "./Login";
+// import Navbar from "./Navbar";
+// import Cards from "./Cards";
+// import Form from "./Form";
+
+const app = () => {
+  return (
+    <div>
+      {/* <Counter /> */}
+      {/* <Navbar /> */}
+      {/* <Cards /> */}
+      {/* <Form /> */}
+      {/* <Form2 /> */}
+      <Login />
+    </div>
+  );
+};
+
+export default app;
